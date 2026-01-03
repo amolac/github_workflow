91d6aa2f1f13dac8771fd5ef6b160d1376bba2e2
Amol Choorappady <amol.choorappady@allianz.com>
2026-01-03 14:52:22 +0530

    app test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Happy NewYear!');
+  res.send('Hello, Github_Actions + EKS + ArgoCD Demo!');
 });
 
 app.listen(port, () => {
